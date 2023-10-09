f4e81912c713b3ac7fe7221d34499f13d53b605e
Azure Serenity <nag13032018@gmail.com>
2023-10-09 22:27:18 +0700

    Update DoiMatKhau.cs

--- CNPM/DoiMatKhau.cs
+++ CNPM/DoiMatKhau.cs
@@ -23,45 +23,58 @@ namespace CNPM
 
         private void btnAccept_Click(object sender, EventArgs e)
         {
-            TaiKhoan taiKhoan = context.TaiKhoans.FirstOrDefault(pass => pass.MatKhau == txtOldPass.Text);
-
-            if (txtOldPass.Text != null)
+            try
             {
-                if (txtOldPass.Text == taiKhoan.MatKhau)
+                // Kiểm tra xem có đủ thông tin trong các ô trống
+                if (string.IsNullOrEmpty(txtOldPass.Text) || string.IsNullOrEmpty(txtNewPass.Text) || string.IsNullOrEmpty(txtNewPass2.Text))
                 {
-                    if (txtNewPass.Text == txtNewPass2.Text)
+                    MessageBox.Show("Vui lòng nhập đủ thông tin!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return; // Không thực hiện thay đổi mật khẩu nếu thiếu thông tin
+                }
+
+                TaiKhoan taiKhoan = context.TaiKhoans.FirstOrDefault(pass => pass.MatKhau == txtOldPass.Text);
+
+                if (txtOldPass.Text != null)
+                {
+                    if (txtOldPass.Text == taiKhoan.MatKhau)
                     {
-                        lbVerify.ForeColor = Color.Green;
-                        lbVerify.Text = "Mật khẩu trùng khớp";
-
-                        taiKhoan.MatKhau = txtNewPass.Text;
-                        context.SaveChanges();
-
-                        MessageBox.Show("Đổi mật khẩu thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                        
-                        this.Close();
-                        // Hiển thị lại Form1 sau khi đổi mật khẩu thành công
-                        Form1 login = Application.OpenForms.OfType<Form1>().FirstOrDefault();
-                        if (login != null)
+                        if (txtNewPass.Text == txtNewPass2.Text)
                         {
-                            login.Show();
+                            lbVerify.ForeColor = Color.Green;
+                            lbVerify.Text = "Mật khẩu trùng khớp";
+
+                            taiKhoan.MatKhau = txtNewPass.Text;
+                            context.SaveChanges();
+
+                            MessageBox.Show("Đổi mật khẩu thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            this.Close();
+                            // Hiển thị lại Form1 sau khi đổi mật khẩu thành công
+                            Form1 login = Application.OpenForms.OfType<Form1>().FirstOrDefault();
+                            if (login != null)
+                            {
+                                login.Show();
+                            }
+                        }
+                        else
+                        {
+                            lbVerify.ForeColor = Color.Red;
+                            lbVerify.Text = "Mật khẩu chưa khớp";
+                            MessageBox.Show("Mật khẩu mới chưa khớp! Vui lòng nhập lại mật khẩu mới.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
                         }
                     }
                     else
                     {
-                        lbVerify.ForeColor = Color.Red;
-                        lbVerify.Text = "Mật khẩu chưa khớp";
-                        MessageBox.Show("Mật khẩu mới chưa khớp! Vui lòng nhập lại mật khẩu mới.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        MessageBox.Show("Mật khẩu đã nhập không khớp với mật khẩu cũ!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                     }
                 }
                 else
                 {
-                    MessageBox.Show("Mật khẩu đã nhập không khớp với mật khẩu cũ!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    MessageBox.Show("Vui lòng nhập mật khẩu cũ!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
             }
-            else
+            catch (Exception ex)
             {
-                MessageBox.Show("Vui lòng nhập mật khẩu cũ!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Đã xảy ra lỗi trong quá trình đổi mật khẩu: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
